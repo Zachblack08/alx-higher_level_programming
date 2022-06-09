bc8fe0ef888db8dfca7beae2bb815a407de83aa9
root <essenowoidee@gmail.com>
2022-06-08 17:02:21 -0700

    added to task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list == [] or my_list is None:
-        return (0)
+        return(0)
     res = 0
-    re2 = 0
+    res2 = 0
     for x, y in my_list:
         res += x * y
         res2 += y
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,4 +2,3 @@
 def common_elements(set_1, set_2):
     common = list(set(set_1) & set(set_2))
     return (common)
-
