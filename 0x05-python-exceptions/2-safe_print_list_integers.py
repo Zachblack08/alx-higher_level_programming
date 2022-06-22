2f8cc5bf1cc671a6e6945e96c58e4d5604ca1f76
root <essenowoidee@gmail.com>
2022-06-21 20:15:17 -0700

    added task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             c += 1
         except (ValueError, TypeError):
             continue
-        print()
-        return c
+    print()
+    return c
