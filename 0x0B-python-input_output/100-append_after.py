d09933f52674d2976e0e3c1a2e562eb7f78a81e9
root <essenowoidee@gmail.com>
2022-07-06 16:25:27 -0700

    added task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -18,4 +18,4 @@ def append_after(filename="", search_string="", new_string=""):
             if search_string in line:
                 text += new_string
     with open(filename, "w") as w:
-         f.writelines(line_list)
+        w.write(text)
